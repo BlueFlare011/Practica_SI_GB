473588435d28f8c3822b3cde434603d3fa490de6
servgo <sergioveguegonzalez@gmail.com>
2023-03-26 23:02:23 +0200

    Carga de datos

--- src/carga_datos.py
+++ src/carga_datos.py
@@ -2,7 +2,7 @@ import sqlite3
 import json
 import csv
 
-con = sqlite3.connect('./PRACTICA1.db')
+con = sqlite3.connect('./sqlite-tools-win32-x86-3410000/PRACTICA1.db')
 cursorObj = con.cursor()
 
 # Devices
@@ -14,8 +14,8 @@ def sql_insert_responsibles(name, phone, rol):
     cursorObj.execute("INSERT INTO RESPONSIBLES VALUES ('" + name + "','" + str(phone) + "','" + rol + "') ")
     con.commit()
 
-def sql_insert_devices(id, ip, localization, rtb_responsible, services, insecures, vulnerabilitys):
-    cursorObj.execute("INSERT INTO DEVICES VALUES ('" + str(id) + "','" + str(ip) + "','" + str(localization) + "','" + str(rtb_responsible) + "','" + str(services) + "','" + str(insecures) + "','" + str(vulnerabilitys) + "') ")
+def sql_insert_devices(id, ip, localization, rtb_responsible, services, insecures, vulnerabilities):
+    cursorObj.execute("INSERT INTO DEVICES VALUES ('" + str(id) + "','" + str(ip) + "','" + str(localization) + "','" + str(rtb_responsible) + "','" + str(services) + "','" + str(insecures) + "','" + str(vulnerabilities) + "') ")
     con.commit()
 
 def sql_insert_devices_port(rtbDevice, port):
@@ -37,3 +37,17 @@ while i < len(lines):
       sql_insert_devices_port(lines[i]["id"],puertos[x])
       x = x+1
    i = i+1
+
+# Alerts
+def sql_insert_alerts(dateTime, sid, message, classification, priority, protocol, origin, destination, port):
+    cursorObj.execute("INSERT INTO ALERTS VALUES ('" + str(dateTime) + "','" + str(sid) + "','" + message + "','" + str(classification) + "','" + str(priority) + "','" + str(protocol) + "','" + str(origin) + "','" + str(destination) + "','" + str(port) + "') ")
+
+arch = open("./data/alerts.csv", "r")
+lines = csv.reader(arch, delimiter=",")
+
+next(lines)
+for line in lines:
+    sql_insert_alerts(line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8])
+con.commit()
+
+con.close()
